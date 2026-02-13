615b1c36b7eb0f535f16b36639eca9be17109477
JuapaTorres <juapa.fc@gmail.com>
2026-02-13 20:21:55 -0300

    repaso 1302

--- PROG AVANZADA PYTHON/Clase2/main.py
+++ PROG AVANZADA PYTHON/Clase2/main.py
@@ -37,4 +37,168 @@ class mascota:
 mascota = mascota("Copito", 14, "Poodle")
 mascota.saludar()
 """
+"""
+class libro:
+    def __init__(self, titulo, autor, anio):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio = anio
+    def mostrar_info(self):
+        print(f"Título: {self.titulo}")
+        print(f"Autor: {self.autor}")
+        print(f"Año de publicación: {self.anio}")
+        print("-" * 30)
+
+libro1 = libro("100 años de soledad", "Gabriel Garcia Marquez", 1967)
+libro2 = libro("El código Da Vinci", "Dan Brown", 2003)
+
+print("Información del libro 1:")
+libro1.mostrar_info()
+
+print("Información del libro 2:")
+libro2.mostrar_info()
+"""
+#Sobrecarga en python usando método *args:
+"""
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self, *args):
+        cantidad = len(args)
+
+        if cantidad == 0:
+            print("Hola")
+        elif cantidad == 1:
+            print(f"Hola, {args[0]}")
+        elif cantidad == 2:
+            print(f"Hola, {args[0]} de {args[1]}")
+        else:
+            print(f"Hola a todos, especialmente a {args[0]}")
+
+usuario = Persona("Admin")
+
+usuario.saludar()
+usuario.saludar("Juan Pablo")
+usuario.saludar("Juan Pablo", "Santiago")
+usuario.saludar("Juan Pablo", "Santiago", "Chile", "Planeta tierra")
+
+"""
+#Sobrecarga de python usando método **kwargs:
+"""
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self, **kwargs):
+        nombre = kwargs.get("nombre")
+        ciudad = kwargs.get("ciudad")
+
+        if nombre and ciudad:
+            print(f"Hola, {nombre} de {ciudad}")
+        elif nombre:
+            print(f"Hola, {nombre}")
+        else:
+            print("Hola")
+
+usuario = Persona("Admin")
+
+usuario.saludar()
+usuario.saludar(nombre = "Juan Pablo")
+usuario.saludar(ciudad = "Santiago", nombre = "Juan Pablo")
+"""
+#Método especial __str__() para personalizar la salida de objetos:
+"""
+class Libro:
+    def __init__(self, titulo, autor, anio):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio = anio
+
+    def __str__(self):
+        return f"Título: {self.titulo} - Autor: {self.autor} - Año: {self.anio}"
+
+libro1 = Libro("El Aleph", "Jorge Luis Borges", 1949)
+libro2 = Libro("1984", "George Orwell", 1949)
+
+print("Presentación clara y profesional:")
+print(libro1)
+print(libro2)
+"""
+#Colaboración entre objetos: ejem de Coche y Motor:
+"""
+class Motor:
+    def __init__(self, tipo, potencia):
+        self.tipo = tipo
+        self.potencia = potencia
+
+    def encender(self):
+        return f"Motor {self.tipo} de {self.potencia} caballos de fuerza encendido."
+    
+class Coche:
+    def __init__(self, marca, modelo, motor):
+        self.marca = marca
+        self.modelo = modelo
+        self.motor = motor
+
+    def arrancar(self):
+        print(f"Iniciando el {self.marca} {self.modelo}...")
+        mensaje_motor = self.motor.encender()
+        print(mensaje_motor)
+        print("El coche está listo para circular.")
+
+mi_motor_v8 = Motor("V8", 450)
+mi_coche = Coche("Ford", "Mustang", mi_motor_v8)
+
+mi_coche.arrancar()
+"""
+#Composición entre objetos: 
+"""
+class Procesador:
+    def __init__(self, marca, velocidad):
+        self.marca = marca
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return f"{self.marca} a {self.velocidad}Ghz"
+    
+class MemoriaRAM:
+    def __init__(self, capacidad, tipo):
+        self.capacidad = capacidad
+        self.tipo = tipo
+
+    def __str__(self):
+        return f"{self.capacidad}GB {self.tipo}"
+    
+class DiscoDuro:
+    def __init__(self, capacidad, tipo):
+        self.capacidad = capacidad
+        self.tipo = tipo
+
+    def __str__(self):
+        return f"{self.capacidad}GB {self.tipo}"
+    
+class Computadora:
+    def __init__(self, marca, procesador, ram, disco):
+        self.marca = marca
+        self.procesador = procesador
+        self.ram = ram
+        self.disco = disco
+
+    def mostrar_info(self):
+        print(f"--- Ficha técnica Computadora {self.marca} ---")
+        print(f"Procesador: {self.procesador}")
+        print(f"Memoria RAM: {self.ram}")
+        print(f"Almacenamiento: {self.disco}")
+        print("." * 35)
+
+intel_i7 = Procesador("Intel Core i7", 3.8)
+corsair_ram = MemoriaRAM(16, "DDR4")
+samsung_ssd = DiscoDuro(512, "SSD NVMe")
+
+mi_pc = Computadora("Dell XPS", intel_i7, corsair_ram, samsung_ssd)
+
+mi_pc.mostrar_info()
+
+"""
 
---
+++ PROG AVANZADA PYTHON/test.py
@@ -0,0 +1,77 @@
+#Ejercicio de Herencia y polimorfismo:
+"""
+class Animal:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def hacer_sonido(self):
+        pass
+
+class Perro(Animal):
+    def hacer_sonido(self):
+        return "Guau guau"
+    
+class Gato(Animal):
+    def hacer_sonido(self):
+        return "Miau miau"
+    
+class Pajaro(Animal):
+    def hacer_sonido(self):
+        return "Pio pio"
+    
+def imprimir_sonidos(animales):
+    print("--- Demostración de Polimorfismo ---")
+    for animal in animales:
+        print(f"{animal.nombre} dice: {animal.hacer_sonido()}")
+
+def es_animal_valido(objeto):
+    if isinstance(objeto, Animal):
+        print(f"Confirmado: El objeto es un Animal (clase o subclase).")
+        return True
+    else:
+        print("Error: El objeto NO pertenece a la jerarquía Animal.")
+
+mis_mascotas = [
+    Perro("Copito", 14),
+    Gato("Panther", 3),
+    Pajaro("Zelda", 1)
+]
+
+imprimir_sonidos(mis_mascotas)
+
+print("\nVerificación de clase:")
+es_animal_valido(mis_mascotas[0])
+"""
+
+#Ejercicio de errores y excepciones:
+
+#DEMO:
+"""
+numero = int(input("Ingresa un número: "))
+resultado = 10 / numero
+print("Resultado:", resultado)
+"""
+#DEMO try/except:
+"""
+def solicitar_numero():
+    try:
+        entrada = input("Por favor, ingrese un número entero: ")
+        numero = int(entrada)
+        print(f"Éxito! El número {numero} es válido.")
+
+    except ValueError:
+        print("Error: Lo que ingresaste no es un número entero válido.")
+        print("Asegúrate de no usar letras ni puntos decimales (ej. usa '10' en lugar de '10.5').")
+
+solicitar_numero()
+"""
+
+# CONVERSIÓN DE UNIDADES CON VALIDACIÓN
+
+def conversor_distancia():
+    FACTOR_MILLAS = 0.621371
+    print("--- Conversor de Kilómetros a Millas ---")
+
+    while True:
+        
